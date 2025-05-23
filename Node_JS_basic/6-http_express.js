5e2cbb7a4dbe22f6d75975d933ec63fcb146d834
Yacine-213 <d.yacine2020@gmail.com>
2025-05-23 18:03:08 +0000

    p

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const port = 1245;
 const app = express();
-module.exports = app;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port);
+app.listen(1245);
+
+module.exports = app;
